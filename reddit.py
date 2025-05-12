0a5b93cbf20bc65608ccc0a6e65dbdf1624722ff
Ishan Shah <shah.ishan111@gmail.com>
2025-05-12 12:53:23 -0400

    pulled subreddit html into python

---
+++ reddit.py
@@ -0,0 +1,7 @@
+import requests 
+from bs4 import BeautifulSoup
+
+
+response = requests.get("https://www.reddit.com/r/Shortsqueeze/")
+text = BeautifulSoup(response.text, 'html.parser')
+print(text.prettify())
